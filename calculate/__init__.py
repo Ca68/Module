ea386194c83442d220dc481e8cb8d7dff03586c0
Ca68 <carine.graff@gmail.com>
2021-06-16 20:17:07 -0500

    This is the first commit for Module

---
+++ calculate/__init__.py
@@ -0,0 +1,8 @@
+import math
+
+def circle_circum(r):
+      print (2 * math.pi * r)
+
+
+def room(l, L):
+    print(l * L)
---
+++ main.py
@@ -0,0 +1,5 @@
+import calculate
+
+calculate.circle_circum(2)
+
+calculate.room(5, 2)
